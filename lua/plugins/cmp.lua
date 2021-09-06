443286039d6cb99054eaefa9ae7cff337296dbc0
collapsoid <collapsly@protonmail.com>
2021-09-06 23:40:07 +0300

    cmp: behavior improvements

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,6 +9,7 @@ local utils = require('utils')
 local set = utils.set
 local func = utils.func
 
+local compare = require('cmp.config.compare')
 
 local WIDE_HEIGHT = 40
 cmp.setup {
@@ -56,28 +57,44 @@ cmp.setup {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.close(),
+        ["<Tab>"] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
         ["<CR>"] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         },
-        ["<Tab>"] = function(fallback)
-            if func.pumvisible() == 1 then
-                func.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
-            elseif require("luasnip").expand_or_jumpable() then
-                func.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true      ), "")
-            else
-                fallback()
-            end
-        end,
-        ["<S-Tab>"] = function(fallback)
-            if func.pumvisible() == 1 then
-                func.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
-            elseif require("luasnip").jumpable(-1) then
-                func.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), ""      )
-            else
-                fallback()
-            end
-        end,
+        -- ["<Tab>"] = function(fallback)
+        --     if func.pumvisible() == 1 then
+        --         func.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
+        --     -- elseif require("luasnip").expand_or_jumpable() then
+        --     --     func.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true      ), "")
+        --     else
+        --         fallback()
+        --     end
+        -- end,
+        -- ["<S-Tab>"] = function(fallback)
+        --     if func.pumvisible() == 1 then
+        --         func.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
+        --     -- elseif require("luasnip").jumpable(-1) then
+        --     --     func.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), ""      )
+        --     else
+        --         fallback()
+        --     end
+        -- end,
+    },
+    sorting = {
+        priority_weight = 2,
+        comparators = {
+            compare.offset,
+            compare.exact,
+            compare.score,
+            compare.kind,
+            compare.sort_text,
+            compare.length,
+            compare.order,
+        }
     },
 
     sources = {
