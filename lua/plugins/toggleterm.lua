6630e709224e252fb0db2d9c9df7cc7f57f58227
collapsoid <collapsly@protonmail.com>
2021-09-08 22:36:34 +0300

    toggleterm: added mappings for blur off terminal window

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,6 +5,9 @@ if not ok then
 end
 
 
+local utils = require('utils')
+local set_map_key = utils.set_map_key
+
 toggleterm.setup {
     size = 20,
     open_mapping = [[<leader>t]],
@@ -12,3 +15,11 @@ toggleterm.setup {
     direction = 'horizontal',
     close_on_exit = true
 }
+
+
+local opts = { noremap = true, silent = true }
+
+set_map_key('t', '<A-h>', [[<C-\><C-n><C-W>h]], opts)
+set_map_key('t', '<A-l>', [[<C-\><C-n><C-W>l]], opts)
+set_map_key('t', '<A-j>', [[<C-\><C-n><C-W>j]], opts)
+set_map_key('t', '<A-k>', [[<C-\><C-n><C-W>k]], opts)
