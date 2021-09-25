b0b8ea06d08fa52426c833292dd836a479cc3429
collapsoid <collapsly@protonmail.com>
2021-09-25 17:07:11 +0300

    gitsigns: deprecated func is changed

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -40,7 +40,7 @@ gitsigns.setup {
     },
     numhl = false,
     linehl = false,
-    watch_index = { interval = 1000 },
+    watch_gitdir = { interval = 1000 },
     sign_priority = 6,
     update_debounce = 200,
     status_formatter = nil
