379f6a4d27667a2e3b265aeb10c0069c9ac83014
collapsoid <collapsly@protonmail.com>
2021-10-31 13:40:14 +0300

    nvim-tree: definition of global variables moved to before the lua plugin "requiring"

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,12 +1,45 @@
+local utils = require('utils')
+local set_map_key = utils.set_map_key
+local g = utils.g
+
+g.nvim_tree_git_hl = 1
+g.nvim_tree_gitignore = 1
+
+g.nvim_tree_show_icons = {
+   folders = 1,
+   folder_arrows= 1,
+   files = 1,
+   git = 1
+}
+
+g.nvim_tree_icons = {
+   default = "",
+   symlink = "",
+   git = {
+      deleted = "",
+      ignored = "◌",
+      renamed = "➜",
+      staged = "S",
+      unmerged = "",
+      unstaged = "",
+      untracked = "U",
+   },
+   folder = {
+      arrow_open = "",
+      arrow_closed = "",
+      default = "",
+      empty = "",
+      empty_open = "",
+      open = "",
+      symlink = ""
+   }
+}
+
 local ok, nvim_tree = pcall(require, 'nvim-tree')
 
 if not ok then
     return
 end
-
-local utils = require('utils')
-local set_map_key = utils.set_map_key
-local g = utils.g
 
 nvim_tree.setup {
     follow = true,
@@ -45,39 +78,6 @@ nvim_tree.setup {
             error = "",
         }
     }
-}
-
-g.nvim_tree_git_hl = 1
-g.nvim_tree_gitignore = 1
-
-g.nvim_tree_show_icons = {
-   folders = 1,
-   folder_arrows= 1,
-   files = 1,
-   git = 1
-}
-
-g.nvim_tree_icons = {
-   default = "",
-   symlink = "",
-   git = {
-      deleted = "",
-      ignored = "◌",
-      renamed = "➜",
-      staged = "S",
-      unmerged = "",
-      unstaged = "",
-      untracked = "U",
-   },
-   folder = {
-      arrow_open = "",
-      arrow_closed = "",
-      default = "",
-      empty = "",
-      empty_open = "",
-      open = "",
-      symlink = ""
-   }
 }
 
 local opts = { noremap = true, silent = true }
