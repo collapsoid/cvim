41599c0ab60590cb89fcadc2c663624cccb96485
collapsoid <collapsly@protonmail.com>
2022-01-27 21:59:13 +0300

    core: organize basic setup commands and some performance improvement is done

--- lua/core/setup.lua
+++ lua/core/setup.lua
@@ -1,26 +1,35 @@
 local utils = require('utils')
-local set = utils.set
 local cmd = utils.cmd
+
+local options = {
+    background = 'dark',
+    completeopt = 'menu,menuone,noselect',
+    cursorline = true,
+    expandtab = true,
+    ignorecase = true,
+    mouse = 'a',
+    number = true,
+    scrolloff = 8,
+    shiftwidth = 4,
+    showmode = false,
+    signcolumn = 'yes',
+    smartindent = true,
+    smarttab = true,
+    softtabstop = 4,
+    syntax = 'on',
+    termguicolors = true,
+    timeoutlen = 300,
+    updatetime = 100,
+    wildignore = '*node_modules/**',
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
 
 -- Basic
 cmd [[
     filetype plugin on
     set nocompatible
     set t_Co=256
-    set completeopt=menuone,noselect
 ]]
-set.number = true
-set.signcolumn = 'yes'
-set.cursorline = true
-set.mouse = 'a'
-set.syntax = 'on'
-set.termguicolors = true
-
--- Tabs
-set.smarttab = true
-set.expandtab = true
-set.shiftwidth = 4
-set.softtabstop = 4
-
--- Appearance
-set.background = 'dark'
