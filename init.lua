0e767b111c3fc635ca6829c5962d8b1eb24038e2
collapsoid <collapsly@protonmail.com>
2022-02-21 22:23:20 +0300

    delete junk

--- init.lua
+++ init.lua
@@ -5,10 +5,7 @@ require('core')
 require('plugins')
 
 
--- cmd [[ colorscheme ghdark ]]
-require('lush')(require('colors.samurai'))
-
--- highlight YankColor ctermfg=59 ctermbg=41 guifg=#989ca9 guibg=#4c5b72
+cmd [[ colorscheme samurai ]]
 
 -- Highlight yank
 cmd [[
